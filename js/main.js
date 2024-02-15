5fcf07b9b053ce6375a3839272c6b7c7653d7b3b
Nikolai <profildh@gmail.com>
2024-02-15 20:56:56 +0300

    Formatting and adding clocks

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Текущее время для странцы приложения
+window.onload = function() {
+  setInterval(function() {
+    // Seconds
+    let seconds = new Date().getSeconds();
+    document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
+
+    // Minutes
+    let minutes = new Date().getMinutes();
+    document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
+
+    // Hours
+    let hours = new Date().getHours();
+    document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
+  }, 1000);
+}
