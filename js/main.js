ba518ae8a1db35cb6247ff7ee7623634b8a2a949
Nikolai <profildh@gmail.com>
2024-02-25 21:35:15 +0300

    First part of the table block

--- js/main.js
+++ js/main.js
@@ -14,3 +14,36 @@ window.onload = function() {
     document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
   }, 1000);
 }
+
+//Блок кода для получения списка
+function getButton(text) {
+	let button = document.createElement("Button");
+	button.textContent = text;
+	return button;
+}
+
+function getDiv(className) {
+	let div = document.createElement("div");
+	div.classList.add(className);
+	return div;
+}
+
+let listArray = [
+	{
+		date: "24.02.2024",
+		customer: "Пупкин Вася",
+		carBrand: "Honda",
+		typesOfWork: "Замена ступичного подшипника",
+		parts: "Ступичный подшипник",
+		price: 2500,
+	},
+];
+
+function getElementTr(index, list) {
+	let elementTr = document.createElement("tr");
+	elementTr.classList.add("tr_items");
+	let elementTd = document.createElement("td");
+	elementTd.classList.add("td_items");
+
+	return elementTr;
+}
